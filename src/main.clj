c1a68dd77608686f65d2ce665651399807e39fb5
Nic Young <nic.youngster@gmail.com>
2016-11-07 15:34:36 +0100

    pulling midid and drawing in quil

--- src/main.clj
+++ src/main.clj
@@ -1,60 +1,49 @@
 (ns main
-  (:require [[quil.core :as q]
-             [midi]
-             [clojure/pprint :as pp]]))
+  (:require [quil.core :as q]
+            [quil.middleware :as m]
+            [midi]))
 
-(use 'midi)
-
-(def launchpad (midi-in))
+(def launchpad (midi/midi-in))
 (def notes (atom {}))
 
 (defn print-handler [notelog]
   (fn [event ts]
     (swap! notelog assoc :last-e event :last-ts ts)))
 
-(midi-handle-events launchpad (print-handler notes))
 
 
 
 
 
-      (with-open [w (clojure.java.io/writer "test.txt")]
-    
-        (.write w (with-out-str 
-          (clojure.pprint/pprint (midi-msg event))))
-      
-        ))))
-
-      ; (or (= (:cmd event) :note-off)
-      ;     (and (not (zero? (:note event)))
-      ;          (zero? (:vel event))))
-      ;   (if-let [note-id (get @notelog (:note event))]
-      ;    (swap! notelog dissoc (:note event))) 
-        ; )))
 
 
 
 
-; (defn setup []
-;   (q/frame-rate 1)                    ;; Set framerate to 1 FPS
-;   (q/background 200))                 ;; Set the background colour to
-                                      ;; a nice shade of grey.
-; (defn draw []
-;   (q/stroke (q/random 255))             ;; Set the stroke colour to a random grey
-;   (q/stroke-weight (q/random 10))       ;; Set the stroke thickness randomly
-;   (q/fill (q/random 255))               ;; Set the fill colour to a random grey
+(defn setup []
+  (q/frame-rate 1)                    ;; Set framerate to 1 FPS
+  (q/background 200)
+  (midi/midi-handle-events launchpad (print-handler notes)))
 
-;   (let [diam (q/random 100)             ;; Set the diameter to a value between 0 and 100
-;         x    (q/random (q/width))       ;; Set the x coord randomly within the sketch
-;         y    (q/random (q/height))]     ;; Set the y coord randomly within the sketch
-;     (q/ellipse x y diam diam)))         ;; Draw a circle at x y with the correct diameter
+(defn draw []
+  (q/stroke (q/random 255))             ;; Set the stroke colour to a random grey
+  (q/stroke-weight (q/random 10))       ;; Set the stroke thickness randomly
+  (q/fill (q/random 255))               ;; Set the fill colour to a random grey
 
-; (q/defsketch example                  ;; Define a new sketch named example
-;   :title "Oh so many grey circles"    ;; Set the title of the sketch
-;   :settings #(q/smooth 2)             ;; Turn on anti-aliasing
-;   :setup setup                        ;; Specify the setup fn
-;   :draw draw                          ;; Specify the draw fn
-;   :size [323 200])                    ;; You struggle to beat the golden ratio
+  (if-let [e (:last-e @notes)]
+    (let [diam   (e :note)  
+          x      (q/random (q/width))
+          y      (q/random (q/height))]
+      (q/ellipse x y diam diam))))
+        
+         
+
+(q/defsketch example                  ;; Define a new sketch named example
+  ; :middleware [m/fun-mode]
+  :title "Oh so many grey circles"    ;; Set the title of the sketch
+  :settings #(q/smooth 2)             ;; Turn on anti-aliasing
+  :setup setup                        ;; Specify the setup fn
+  :draw draw                          ;; Specify the draw fn
+  :size [323 200])                    ;; You struggle to beat the golden ratio
 
 
 
