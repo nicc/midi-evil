aca1c99e9962c24025266e76a190b52791156d4c
Nic Young <nic.youngster@gmail.com>
2016-11-08 15:15:54 +0100

    cleaned up comments a little and increased sketch window size

--- src/main.clj
+++ src/main.clj
@@ -32,7 +32,7 @@
     (q/ellipse x y diam diam)))
 
 (defn setup []
-  (q/frame-rate 1)
+  (q/frame-rate 60)
   (q/background 200)
   (midi/midi-handle-events launchpad (record-events-fn :launchpad))
     {})
@@ -52,8 +52,6 @@
   (doseq [[n e] (seq (state :launchpad))]
     ; (throw (Exception. (with-out-str (clojure.pprint/pprint e))))
     (draw-note-circle n e)))
-        
-         
 
 (q/defsketch example                  ;; Define a new sketch named example
   :middleware [m/fun-mode]
@@ -62,11 +60,8 @@
   :setup setup                        ;; Specify the setup fn
   :update update-state
   :draw draw                          ;; Specify the draw fn
-  :size [323 200])                    ;; You struggle to beat the golden ratio
+  :size [646 400])                    ;; You struggle to beat the golden ratio
 
-
-
-  ; (midi-handle-events launchpad adam-handler)
 
 ; (defn adam-handler [inst]
 ;  (let [notes* (atom {})]
@@ -81,25 +76,3 @@
 
 ;        (if-let [note-id (get @notes* (:note event))]
 ;          (ctl note-id :gate 0))))))
-
-
-; (defn setup []
-;   (q/frame-rate 1)                    ;; Set framerate to 1 FPS
-;   (q/background 200))                 ;; Set the background colour to
-;                                       ;; a nice shade of grey.
-; (defn draw []
-;   (q/stroke (q/random 255))             ;; Set the stroke colour to a random grey
-;   (q/stroke-weight (q/random 10))       ;; Set the stroke thickness randomly
-;   (q/fill (q/random 255))               ;; Set the fill colour to a random grey
-
-;   (let [diam (q/random 100)             ;; Set the diameter to a value between 0 and 100
-;         x    (q/random (q/width))       ;; Set the x coord randomly within the sketch
-;         y    (q/random (q/height))]     ;; Set the y coord randomly within the sketch
-;     (q/ellipse x y diam diam)))         ;; Draw a circle at x y with the correct diameter
-
-; (q/defsketch example                  ;; Define a new sketch named example
-;   :title "Oh so many grey circles"    ;; Set the title of the sketch
-;   :settings #(q/smooth 2)             ;; Turn on anti-aliasing
-;   :setup setup                        ;; Specify the setup fn
-;   :draw draw                          ;; Specify the draw fn
-;   :size [323 200])                    ;; You struggle to beat the golden ratio
