0993a64c6b9e0b6a15612e9f49b04d3bdb2d8c7c
Nic Young <nic.youngster@gmail.com>
2016-11-08 15:06:19 +0100

    can get signals from midi device onto screen, yay

--- target/main.clj
+++ target/main.clj
@@ -1,36 +1,66 @@
 (ns main
   (:require [quil.core :as q]
+            [quil.middleware :as m]
             [midi]))
 
 (def launchpad (midi/midi-in))
-(def notes (atom {}))
+(def device-events (atom {:launchpad []}))
+(def devices {:launchpad launchpad}) 
 
-(defn print-handler [notelog]
+(defn record-events-fn [device-name]
   (fn [event ts]
-    (swap! notelog assoc :last-e event :last-ts ts)))
+    (swap! ; TODO: add timestamp to event
+      device-events
+      assoc device-name (conj (@device-events device-name) event))))
+
+(defn pull-events! [device-name]
+  (let [events (@device-events device-name)]
+    (swap!
+      device-events
+      assoc device-name [])
+    events))
+
+(defn generate-note-map [events]
+  (->> events
+    (reduce #(-> %1 (conj (:note %2)) (conj %2)) [])
+    (apply hash-map)))
+
+(defn draw-note-circle [note _]
+  (let [diam (q/random note)       
+        x    (q/random (q/width))
+        y    (q/random (q/height))]
+    (q/ellipse x y diam diam)))
 
 (defn setup []
-  (q/frame-rate 1)                    ;; Set framerate to 1 FPS
+  (q/frame-rate 1)
   (q/background 200)
-  (midi/midi-handle-events launchpad (print-handler notes)))
+  (midi/midi-handle-events launchpad (record-events-fn :launchpad))
+    {})
 
-(defn draw []
-  (q/stroke (q/random 255))             ;; Set the stroke colour to a random grey
-  (q/stroke-weight (q/random 10))       ;; Set the stroke thickness randomly
-  (q/fill (q/random 255))               ;; Set the fill colour to a random grey
+(defn update-state [state]
+  (->>
+    (pull-events! :launchpad)
+    (generate-note-map)
+    (hash-map :launchpad)
+    (merge state)))
 
-  (if-let [e (:last-e @notes)]
-    (let [diam   (e :note)  
-          x      (q/random (q/width))
-          y      (q/random (q/height))]
-      (q/ellipse x y diam diam))))
+(defn draw [state]
+  (q/stroke 0)  
+  (q/stroke-weight 4)
+  (q/fill (q/random 255))
+
+  (doseq [[n e] (seq (state :launchpad))]
+    ; (throw (Exception. (with-out-str (clojure.pprint/pprint e))))
+    (draw-note-circle n e)))
         
          
 
 (q/defsketch example                  ;; Define a new sketch named example
+  :middleware [m/fun-mode]
   :title "Oh so many grey circles"    ;; Set the title of the sketch
   :settings #(q/smooth 2)             ;; Turn on anti-aliasing
   :setup setup                        ;; Specify the setup fn
+  :update update-state
   :draw draw                          ;; Specify the draw fn
   :size [323 200])                    ;; You struggle to beat the golden ratio
 
